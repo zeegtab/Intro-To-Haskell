10e1465fc530b869b9cecb0ea219aa43be190426
zeegtab <30591062+zeegtab@users.noreply.github.com>
2021-07-05 00:22:11 +0100

    Added simple functions involving lists

--- 03_lists.hs
+++ 03_lists.hs
@@ -1,1 +1,43 @@
 module Lists where
+
+import Prelude hiding(sum)
+import Data.List hiding(sum)
+
+-- SUM
+sum :: [Int] -> Int
+sum [] 
+    = 0
+sum (x : xs)
+    = x + sum xs
+
+-- PRECEDES (pattern matching)
+precedes :: String -> String -> Bool
+precedes (s : []) (t : [])
+    = s < t
+precedes [] _ 
+    = True
+precedes _ []
+    = False
+precedes (s : s') (t : t')
+    | s == t    = precedes s' t'
+    | otherwise = s < t
+
+-- PERMUTATIONS OF A LIST
+perms :: Eq a => [a] -> [[a]]
+perms []
+    = [[]]
+perms xs
+    = [ x : ps | x <- xs, ps <- perms (xs \\ [x])]
+
+-- POSITION
+pos :: Char -> String -> Int
+pos c (s : s')
+    | c == s    = 0
+    | otherwise = 1 + pos c s'
+
+-- PREFIXES
+prefixes :: [t] -> [[t]]
+prefixes [] 
+    = []
+prefixes (t : ts)
+    = [t : ps | ps <- prefixes ts]
